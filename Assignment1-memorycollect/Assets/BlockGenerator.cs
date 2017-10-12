2bec9d6171bfad6bd93d10b584cb48a0e2eff28e
mrtraub <mitch.traub@gmail.com>
2017-10-12 13:53:57 -0400

    HW1 - object generation
    Collect the matching balls

---
+++ Assignment1-memorycollect/Assets/BlockGenerator.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockGenerator : MonoBehaviour {
+	public Collector player;
+	public GameObject item;
+	public GameObject dirlight;
+	public int minimumAmount;
+	public int maximumAmount;
+	public bool deploy = false;
+	public bool deploy2 = false;
+	public GameObject win;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckInventory ();
+	}
+
+	public void CheckInventory() {
+
+		if (player.inventory.Count % 2 == 1) {
+			dirlight.SetActive (false);
+		}
+		if (player.inventory.Count % 2 == 0){
+			dirlight.SetActive (true);
+		}
+		if (player.inventory.Count == 2) {
+			if (deploy == false) {
+				int numberOfItemsToDrop = Random.Range (minimumAmount, maximumAmount);
+				for (int i = 0; i < numberOfItemsToDrop; i++) {
+					GameObject showerItem = (GameObject)Instantiate (item, transform);
+					showerItem.transform.Translate (Random.Range (-15.0f, 15.0f), 10, Random.Range (-15.0f, 15.0f));
+					showerItem.SetActive (true);
+				}
+				deploy = true;
+			}
+		}
+		if (player.inventory.Count == 4) {
+			if (deploy2 == false) {
+				int numberOfItemsToDrop = Random.Range (minimumAmount, maximumAmount);
+				for(int i = 0; i < numberOfItemsToDrop; i++) {
+					GameObject showerItem = (GameObject)Instantiate (item, transform);
+					showerItem.transform.Translate (Random.Range (-15.0f, 15.0f), 10, Random.Range (-15.0f, 15.0f));
+					showerItem.SetActive(true);
+				}
+				deploy2 = true;
+			}
+		}
+		if (player.inventory.Count == 6) {
+			win.SetActive (true);
+		}
+
+	}
+}
---
+++ Assignment1-memorycollect/Assets/Collector.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collector : MonoBehaviour {
+	public List<string> inventory;
+	private bool isR = false;
+	private bool isB = false;
+	private bool isM = false;
+	public GameObject lose;
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Collectable") {
+			other.GetComponent<Collectable> ().Collect ();
+			inventory.Add (other.GetComponent<Collectable> ().description);
+			Debug.Log ("collect");
+		}
+		if (other.name == "Red Sphere") {
+			Debug.Log ("got a red");
+			if (isB == true || isM == true) {
+				lose.SetActive (true);
+				Debug.Log ("you lose");
+			} else if (isR == true) {
+				isR = false;
+				Debug.Log ("two reds");
+			} else {
+				isR = true;
+				Debug.Log ("first reds");
+			}
+			
+		}
+		if (other.name == "Blue Sphere") {
+			if (isR == true || isM == true) {
+				lose.SetActive (true);
+
+			} else if (isB == true) {
+				isB = false;
+			} else
+				isB = true;
+		}
+		if (other.name == "Mauve Sphere") {
+			if (isB == true || isR == true) {
+				lose.SetActive (true);
+			} else if (isM == true) {
+				isM = false;
+			} else
+				isM = true;
+		}
+
+		//GetComponent<LightUnlock> ().CheckInventory ();
+	}
+
+}
